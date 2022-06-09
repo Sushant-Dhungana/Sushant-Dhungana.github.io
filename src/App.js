ed8788a19d0801f1eae037187ef73b7387fd4ee0
Sushant-Dhungana <sushantdhungana29@gmail.com>
2022-06-09 09:58:28 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>learn react</h1>
+      <h3>Sushant Dhungana</h3>
     </div>
   );
 }
